f713e337939fb701da5de561c20c6659f033ad7a
Allen Farias <allenfarias29@gmail.com>
2023-10-28 01:25:46 -0700

    Displays the passed array prop of 20 pokemon

---
+++ src/PokeList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function PokeList({ pokemon }) {
+
+    return (
+        <div>
+            {pokemon.map(p => (
+                <div key={p}>
+                    {p}
+                </div>
+            ))}
+        </div>
+    );
+}
